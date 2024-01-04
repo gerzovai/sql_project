564facd27ddecab2b7a27f8cbb9552fd9d5ae0f7
Ivana Geržová <gerzova.i8@gmail.com>
2024-01-04 09:38:01 +0100

    vytvorena primarni tabulka

--- sql_project.sql
+++ sql_project.sql
@@ -1,6 +1,3 @@
-SELECT *
-FROM czechia_payroll_value_type AS cpvt;
-
 -- primární tabulka --
 /*
 v základní payroll mám kody - přiřadit k nim popis
@@ -10,51 +7,70 @@ vybrat jen potřebné sloupce a value_type_code omezit na 5958,
 
 zkusit propojit payroll a price podle DATA za rok, obe napřed zprůměrovat
 
-CREATE TABLE t_ivana_gerzova_project_SQL_primary_final AS
-	(
+drop TABLE t_ivana_gerzova_project_SQL_primary_final;
 	
 	
--- tabulka mezd--
 	
-SELECT 
-	cp.id,
-	round(avg(cp.value), 2) AS average_value,
-	cpvt.name AS value_type,
-	cpu.name AS unit,
-	cpc.name AS calculation,
-	cpib.name AS industry_branch,
-	cp.payroll_year
-FROM czechia_payroll AS cp 
-JOIN czechia_payroll_value_type AS cpvt
-	ON cp.value_type_code = cpvt.code 
-JOIN czechia_payroll_unit AS cpu 
-	ON cp.unit_code = cpu.code 
-JOIN czechia_payroll_calculation AS cpc 
-	ON cp.calculation_code = cpc.code 
-JOIN czechia_payroll_industry_branch AS cpib 
-	ON cp.industry_branch_code = cpib.code
-WHERE value_type_code = 5958
-GROUP BY industry_branch_code, payroll_year;
-
--- tabulka cen --
-
-SELECT 
-	cpc.name AS category,
-	cp.value,
-	avg( YEAR (cp.date_from)) AS date_from
-	-- YEAR (cp.date_to) AS date_to, --
-FROM czechia_price AS cp 
-JOIN czechia_price_category AS cpc 
-	ON cp.category_code = cpc.code 
-LEFT JOIN czechia_region AS cr 
-	ON cp.region_code = cr.code
-WHERE region_code IS NULL 
-GROUP BY cp.region_code, cp.category_code, YEAR (cp.date_from) ;
+-- tabulka mezd--	
+CREATE TABLE t_ivana_gerzova_project_SQL_primary_final AS (
+	WITH edit_czechia_payroll AS (	
+		SELECT 
+			round(avg(cp.value), 2) AS average_value,
+			cpvt.name AS value_type,
+			cpu.name AS unit,
+			cpib.name AS industry_branch,
+			cp.payroll_year
+		FROM czechia_payroll AS cp 
+		LEFT JOIN czechia_payroll_value_type AS cpvt
+			ON cp.value_type_code = cpvt.code 
+		LEFT JOIN czechia_payroll_unit AS cpu 
+			ON cp.unit_code = cpu.code  
+		LEFT JOIN czechia_payroll_industry_branch AS cpib 
+			ON cp.industry_branch_code = cpib.code
+		WHERE value_type_code = 5958
+		GROUP BY industry_branch_code, payroll_year
+	),
+	edit_czechia_price AS (
+		SELECT 
+			cpc.name AS category,
+			round(avg (cp2.value), 2) AS average_price,
+			YEAR (cp2.date_from) AS date_year
+		FROM czechia_price AS cp2 
+		LEFT JOIN czechia_price_category AS cpc 
+			ON cp2.category_code = cpc.code 
+		LEFT JOIN czechia_region AS cr 
+			ON cp2.region_code = cr.code
+		WHERE cp2.region_code IS NULL 
+		GROUP BY category, date_year
+	)
+	SELECT *
+	FROM edit_czechia_payroll AS ecp
+	CROSS JOIN edit_czechia_price AS ecp2
+		ON ecp.payroll_year = ecp2.date_year
+);
+	
+SELECT * FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf;
 	
 SELECT * FROM czechia_price_category AS cpc 
 	
 SELECT *
 FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf ;
+
+
+SELECT 
+	cpc.name AS category,
+	round(avg (cp2.value), 2) AS average_price,
+	YEAR (cp2.date_from) AS date_year
+FROM czechia_price AS cp2 
+LEFT JOIN czechia_price_category AS cpc 
+	ON cp2.category_code = cpc.code 
+LEFT JOIN czechia_region AS cr 
+	ON cp2.region_code = cr.code
+WHERE cp2.region_code IS NULL 
+GROUP BY category, date_year
+;
+
+
 
 -- otázka 1 --
 /*

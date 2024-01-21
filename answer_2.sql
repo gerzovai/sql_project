52ca7b669a1eb67caabf55d7ca82ce8d8eca5007
Ivana Geržová <gerzova.i8@gmail.com>
2024-01-21 16:12:45 +0100

    odpoved 2

---
+++ answer_2.sql
@@ -0,0 +1,13 @@
+-- otázka 2 --
+/*Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?*/
+
+SELECT 
+	industry_branch,
+	category,
+	payroll_year,
+	average_value / average_price AS amount
+FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf 
+WHERE (category LIKE 'Mléko%' OR category LIKE 'Chléb%')
+	AND (payroll_year = 2006 OR payroll_year = 2018)
+	AND industry_branch IS NOT NULL 
+ORDER BY industry_branch, category ;

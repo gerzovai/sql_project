00e51024f7f6706ce08d25ad1fcf346ed450920b
Ivana Geržová <gerzova.i8@gmail.com>
2024-01-21 16:13:13 +0100

    odpoved 3

---
+++ answer_3.sql
@@ -0,0 +1,12 @@
+-- otázka 3 --
+/*Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?*/
+
+SELECT 
+	tigpspf.category,
+	avg(((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price) * 100) AS percentage_increase
+FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf 
+JOIN t_ivana_gerzova_project_sql_primary_final AS tigpspf2 
+	ON tigpspf.category = tigpspf2.category 
+	AND tigpspf.date_year + 1  = tigpspf2.date_year
+GROUP BY category 
+ORDER BY percentage_increase ;

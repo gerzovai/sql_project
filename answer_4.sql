36af342d6eed4d1dfc7633dd7f4566177cd417b8
Ivana Geržová <gerzova.i8@gmail.com>
2024-01-22 17:22:11 +0100

    uprava - serazeni sestupne

--- answer_4.sql
+++ answer_4.sql
@@ -6,11 +6,12 @@ SELECT
 	tigpspf2.payroll_year,
 	((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value ) * 100 AS percentage_increase_value, 
 	avg ((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price ) * 100 AS percentage_increase_price,
-	(((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value ) * 100) - (avg ((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price ) * 100) AS increase_difference
+	round( (avg ((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price ) * 100) - (((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value ) * 100), 2)AS increase_difference
 FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf 
 JOIN t_ivana_gerzova_project_sql_primary_final AS tigpspf2  
 	ON tigpspf.category = tigpspf2.category  
 	AND tigpspf.date_year + 1  = tigpspf2.date_year
 WHERE tigpspf.industry_branch IS NULL 
 	AND tigpspf2.industry_branch IS NULL
-GROUP BY tigpspf.payroll_year ;
+GROUP BY tigpspf.payroll_year 
+ORDER BY increase_difference DESC ;

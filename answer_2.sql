36fceeed6ab6ba1086148dc2db7ed108c865cca8
gerzovai <gerzova.i8@gmail.com>
2024-02-26 08:23:27 +0100

    uprava finalni

--- answer_2.sql
+++ answer_2.sql
@@ -5,9 +5,9 @@ SELECT
 	industry_branch,
 	category,
 	payroll_year,
-	floor( average_value / average_price) AS amount
+	FLOOR(average_value / average_price) AS amount
 FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf 
-WHERE (category LIKE 'Mléko%' OR category LIKE 'Chléb%')
+WHERE (category LIKE 'Mléko polotučné pasterované' OR category LIKE 'Chléb konzumní kmínový')
 	AND (payroll_year = 2006 OR payroll_year = 2018)
 	AND industry_branch IS NOT NULL 
 ORDER BY industry_branch, category ;

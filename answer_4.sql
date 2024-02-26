d8f2ecca444a8a85a4c6c792546dbbadf543dce4
gerzovai <gerzova.i8@gmail.com>
2024-02-26 08:36:10 +0100

    uprava finalni

--- answer_4.sql
+++ answer_4.sql
@@ -4,9 +4,9 @@
 SELECT 
 	tigpspf.payroll_year,
 	tigpspf2.payroll_year,
-	((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value ) * 100 AS percentage_increase_value, 
-	avg ((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price ) * 100 AS percentage_increase_price,
-	round( (avg ((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price ) * 100) - (((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value ) * 100), 2)AS increase_difference
+	((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value) * 100 AS percentage_increase_value, 
+	AVG((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price) * 100 AS percentage_increase_price,
+	ROUND((AVG((tigpspf2.average_price - tigpspf.average_price) / tigpspf.average_price) * 100) - (((tigpspf2.average_value - tigpspf.average_value) / tigpspf.average_value) * 100), 2) AS increase_difference
 FROM t_ivana_gerzova_project_sql_primary_final AS tigpspf 
 JOIN t_ivana_gerzova_project_sql_primary_final AS tigpspf2  
 	ON tigpspf.category = tigpspf2.category  
